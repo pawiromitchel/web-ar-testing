5d0e8e1cf4f066311066c5fc2f0c7413a049c36e
Mitchel Pawirodinomo <pawiromitchel@gmail.com>
2020-09-05 09:38:14 -0300

    database opgezet

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require("fs");
+const path = require("path");
+const helmet = require('helmet');
+
+const jwt = require('./server/middlewares/jwt');
+const publicRoute = require('./server/config/jwt.json').publicRoute;
+
+const app = express();
+app.use(logger('dev'));
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors({ credentials: true, origin: true }));
+
+// all requests will go through this middleware first
+app.use(function (req, res, next) {
+  if (req.method == "POST" && (publicRoute.indexOf(req.url) > -1)) {
+    // route normally
+    next();
+  } else {
+    // verify if the requester is authenticated
+    jwt.verifyToken(req, res, next);
+  }
+});
+
+// Auto load all our routes into the application.
+const normalizedPath = path.join(__dirname, "./server/routes");
+fs.readdirSync(normalizedPath).forEach(function(file) {
+  require("./server/routes/" + file)(app);
+});
+
+// When a route doesnt exist, show the requester this message
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.',
+}));
+
+module.exports = app;
